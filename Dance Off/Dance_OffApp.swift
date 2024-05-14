738da689f02e77551d9b294f684e5ecc6c26fdad
Balya Elfata <balyaelfata@yahoo.com>
2024-05-14 20:08:30 +0700

    Initial Commit

---
+++ Dance Off/Dance_OffApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dance_OffApp.swift
+//  Dance Off
+//
+//  Created by Balya Elfata on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Dance_OffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
