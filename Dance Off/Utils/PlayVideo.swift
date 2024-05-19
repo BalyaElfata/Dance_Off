ef5a63d0aa3e9e98438a81384f1b50df924b8c3a
Balya Elfata <balyaelfata@yahoo.com>
2024-05-20 00:44:06 +0700

    test video randomization

--- Dance Off/Utils/PlayVideo.swift
+++ Dance Off/Utils/PlayVideo.swift
@@ -4,9 +4,11 @@ import AVKit
 struct PlayVideo: View {
     @State private var showSidebar = true
     
-    let avPlayer = AVPlayer(url:  Bundle.main.url(forResource: "video", withExtension: "mp4")!)
+    let videos = ["video","video2","video3","video4","video5"]
     
     var body: some View {
+        let avPlayer = AVPlayer(url:  Bundle.main.url(forResource: videos[Int.random(in: videos.indices)], withExtension: "mp4")!)
+        
         ZStack{
             Color(.orange)
             NavigationSplitView {
@@ -28,13 +30,13 @@ struct PlayVideo: View {
                 }
             } detail: {
                 VideoPlayer(player: avPlayer)
-//                    .frame(height: 500)
+                    .frame(height: 500)
                     .onAppear {
                         avPlayer.play()
                         avPlayer.isMuted = true
                     }
                     .onDisappear {
-                        /// play your turn
+                        // play your turn
                     }
             }
         }
