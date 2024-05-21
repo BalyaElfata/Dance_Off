e0a113369fe53e60524a051a14e1f5f52f0c6293
Balya Elfata <balyaelfata@yahoo.com>
2024-05-21 21:10:50 +0700

    MLView test

--- Dance Off/Views/ContentView.swift
+++ Dance Off/Views/ContentView.swift
@@ -8,18 +8,115 @@
 import SwiftUI
 import CoreML
 import Vision
+import AVFoundation
+import AVKit
 
 struct ContentView: View {
-    var body: some View {
+    @ObservedObject var predictionVM = PredictionViewModel()
+    
+    @State private var avPlayer: AVPlayer?
+    @State private var currentVideoIndex = 0
+    
+    @State var avgScore: Double?
+    
+    let videos = ["video2", "video3", "video4", "video5"]
+    
+    var switchCamera: some View {
+        HStack {
+            Button {
+                predictionVM.videoCapture.toggleCameraSelection()
+            } label: {
+                Image(systemName: "arrow.triangle.2.circlepath.camera.fill")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+            }
+            .padding(.leading)
+            
+            Spacer()
+        }
+    }
+    
+    var predictionLabels: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-                .font(Font.custom("Samurai Blast", size: 32))
+            switchCamera
+            Spacer()
+        }
+    }
+    
+    var body: some View {
+        Text("\(predictionVM.predicted)")
+            .font(Font.custom("Samurai Blast", size: 40))
+            .foregroundStyle(.purple)
+        
+        Text("Confidence: \(predictionVM.confidence)")
+            .font(Font.custom("Samurai Blast", size: 40))
+            .foregroundStyle(.blue)
+        HStack {
+            // Video Player
+            if let player = avPlayer {
+                VideoPlayer(player: player)
+                    .frame(width: 500)
+                    .onAppear {
+                        player.play()
+                        player.isMuted = true
+                    }
+            } else {
+                Text("Loading video...")
+            }
+            ZStack {
+                Color(.black)
+                
+                predictionLabels
+            }
         }
         .padding()
+        .onAppear {
+            playNextVideo()
+            predictionVM.updateUILabels(with: .startingPrediction)
+        }
+        .onReceive(
+            NotificationCenter
+                .default
+                .publisher(for: UIDevice.orientationDidChangeNotification)
+        ) { _ in
+            predictionVM.videoCapture.updateDeviceOrientation()
+        }
     }
+    
+    private func playNextVideo() {
+        guard !videos.isEmpty else { return }
+        
+        let videoName = videos[currentVideoIndex]
+        
+        if let url = Bundle.main.url(forResource: videoName, withExtension: "mp4") {
+            avPlayer = AVPlayer(url: url)
+            
+            let playerItem = AVPlayerItem(url: url)
+            avPlayer?.replaceCurrentItem(with: playerItem)
+            avPlayer?.play()
+            
+            NotificationCenter.default.addObserver(
+                forName: .AVPlayerItemDidPlayToEndTime,
+                object: playerItem,
+                queue: .main
+            ) { [self] _ in
+                self.videoDidFinishPlaying()
+            }
+        } else {
+            print("Error: Video file \(videoName) not found")
+        }
+    }
+    
+    private func videoDidFinishPlaying() {
+        currentVideoIndex = (currentVideoIndex + 1) % videos.count
+        playNextVideo()
+    }
+    
+    private func danceScore(_ actionPrediction: ActionPrediction) -> Double? {
+        let score = actionPrediction.confidence
+        return score
+    }
+
 }
 
 #Preview {
