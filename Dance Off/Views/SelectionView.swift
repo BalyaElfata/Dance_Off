576293a4870ef1740cef860950c8652191350e01
Balya Elfata <balyaelfata@yahoo.com>
2024-05-25 22:07:10 +0700

    Added Song Selection Page

--- Dance Off/Views/SelectionView.swift
+++ Dance Off/Views/SelectionView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct SelectionView: View {
+    var songs = SongList.getSongData()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            Color(hex: 0xF1F17C, opacity: 0.8)
+                .ignoresSafeArea()
+            VStack {
+                Text("Song List")
+                    .font(.largeTitle)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                ScrollView(.vertical) {
+                    LazyVStack(spacing: 25) {
+                        ForEach(songs.indices, id: \.self) { index in
+                            let song = songs[index]
+                            NavigationLink (destination: MLView(song: song.fileName)) {
+                                SongView(song: song)
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
