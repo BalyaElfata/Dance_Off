fcf04c110f0874ef01b0fec76bb80469f345b42d
Balya Elfata <balyaelfata@yahoo.com>
2024-05-26 21:44:23 +0700

    changed Home View appearance

--- Dance Off/Views/HomeView.swift
+++ Dance Off/Views/HomeView.swift
@@ -2,46 +2,48 @@ import SwiftUI
 import AVFoundation
 
 struct HomeView: View {
+    @State var viewDidLoad = false
     @State var isTapped = false
     @State var isScaled = false
+    @State var isRotated = false
     @Environment(\.backgroundMaterial) var backgroundMaterial
     
     var body: some View {
         NavigationStack {
-            // background
             ZStack {
+                // background
                 Image("background")
                     .resizable()
                     .scaledToFill()
                     .ignoresSafeArea()
-                Color(hex: 0xF1F17C, opacity: 0.8)
+                Color(hex: 0xF2BB05, opacity: 0.8)
                     .ignoresSafeArea()
-//                Circle()
-//                    .frame(height: 1350)
-//                    .foregroundColor(Color(hex: 0xECEC46, opacity: 1))
-//                Circle()
-//                    .frame(height: 1100)
-//                    .foregroundColor(Color(hex: 0xDEDE17, opacity: 1))
-//                Circle()
-//                    .frame(height: 850)
-//                    .foregroundColor(Color(hex: 0xCBCB15, opacity: 1))
+                
                 VStack (alignment: .center){
                     //Camera Button
-                    NavigationLink (destination: SelectionView()) {
-                        Image("logo")
-                            .resizable()
-                            .frame(width: 600, height: 600)
-                            .shadow(color: Color(red: 0.13, green: 0.05, blue: 0.27).opacity(0.3), radius: 15, x: 0, y: 0)
+                    NavigationLink (destination: SongSelectionView()) {
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(Color(hex: 0x935FA7, opacity: 1))
+                                .frame(width: 400, height: 240)
+                                .cornerRadius(40)
+                                .shadow(color: Color(red: 0.13, green: 0.05, blue: 0.27).opacity(0.3), radius: 15, x: 0, y: 0)
+                            Image(systemName: "play.fill")
+                                .resizable()
+                                .frame(width: 120, height: 100)
+                                .foregroundColor(.white)
+                        }
+                        .scaleEffect(viewDidLoad ? 1 : 0)
                     }
-                    .simultaneousGesture(TapGesture().onEnded {
-                                                UIImpactFeedbackGenerator(style: .rigid).impactOccurred()
-                                                Sounds.playSound(sound: "coin3", type: "wav")
-                    })
                 }
             }
         }
+        // hides back button from splash screen
+        .navigationBarBackButtonHidden(true)
+        
 //         Play Music
         .onAppear() {
+            withAnimation(.easeInOut(duration: 1)) {viewDidLoad = true}
             Sounds.playMusic(music: "bgMusic", type: "mp3")
         }
     }
