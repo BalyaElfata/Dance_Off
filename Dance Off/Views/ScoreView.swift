8aac8fa5b6c414d47fc1c475724e302305d80513
Balya Elfata <balyaelfata@yahoo.com>
2024-05-26 21:43:45 +0700

    changed score view appearance

--- Dance Off/Views/ScoreView.swift
+++ Dance Off/Views/ScoreView.swift
@@ -8,18 +8,20 @@
 import SwiftUI
 
 struct ScoreView: View {
+    @Binding var totalScore: Double
+    
     var body: some View {
         ZStack {
             Image("background")
                 .resizable()
                 .scaledToFill()
                 .ignoresSafeArea()
-            Color(hex: 0xF1F17C, opacity: 0.8)
+            Color(hex: 0xF2BB05, opacity: 0.8)
                 .ignoresSafeArea()
             VStack {
                 ZStack {
                     Rectangle()
-                        .fill(.white)
+                        .fill(Color(hex: 0xB594B6, opacity: 1))
                         .frame(width: 440, height: 280)
                         .cornerRadius(40)
                         .shadow(radius: 8)
@@ -28,33 +30,37 @@ struct ScoreView: View {
                             Image(systemName: "star.fill")
                                 .resizable()
                                 .frame(width:50, height: 50)
-                                .foregroundColor(.yellow)
+                                .foregroundColor(Color(hex: 0xD74E09, opacity: 1))
                             Image(systemName: "star.fill")
                                 .resizable()
                                 .frame(width:50, height: 50)
-                                .foregroundColor(.yellow)
+                                .foregroundColor(Color(hex: 0xD74E09, opacity: 1))
                             Image(systemName: "star.fill")
                                 .resizable()
                                 .frame(width:50, height: 50)
-                                .foregroundColor(.yellow)
+                                .foregroundColor(Color(hex: 0xD74E09, opacity: 1))
                         }
                         .padding()
                         Text("Score: 80")
                             .font(Font.custom("Samurai Blast", size: 60))
-                            .foregroundColor(Color(hex: 0x24272B, opacity: 1))
+//                            .foregroundColor(Color(hex: 0x24272B, opacity: 1))
                     }
                 }
-                Image(systemName: "arrowshape.backward.fill")
-                    .resizable()
-                    .frame(width: 40, height: 40)
-                    .background(
-                        Rectangle()
-                            .fill(Color(hex: 0xB594B6, opacity: 0.8))
-                            .frame(width: 160, height: 80)
-                            .cornerRadius(20)
-                            .shadow(radius: 4)
-                    )
-                    .padding(.top, 80)
+                // Back Button to Song Selection View
+                NavigationLink (destination: SongSelectionView()) {
+                    Image(systemName: "arrowshape.backward.fill")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(.black)
+                        .background(
+                            Rectangle()
+                                .fill(Color(hex: 0x935FA7, opacity: 0.8))
+                                .frame(width: 160, height: 80)
+                                .cornerRadius(20)
+                                .shadow(radius: 4)
+                        )
+                        .padding(.top, 80)
+                }
             }
         }
     }
