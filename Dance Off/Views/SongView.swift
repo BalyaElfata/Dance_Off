1fe382f3929beaeb6fadec3e3666b9bc672290df
Balya Elfata <balyaelfata@yahoo.com>
2024-05-28 14:43:32 +0700

    adjusted song card display

--- Dance Off/Views/SongView.swift
+++ Dance Off/Views/SongView.swift
@@ -6,11 +6,6 @@ struct SongView: View {
 
     var body: some View {
         ZStack {
-//            Rectangle()
-//                .fill(Color.white)
-//                .frame(width: 720, height: 200)
-//                .cornerRadius(40)
-//                .shadow(radius: 8)
             HStack() {
                 Image("logo")
                     .resizable()
@@ -19,8 +14,7 @@ struct SongView: View {
                     .padding(.trailing, 40)
                 Text(song.songName)
                     .foregroundColor(Color(hex: 0xF0F0C9, opacity: 1))
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .font(Font.custom("Cherry Bomb One", size: 30))
                 Spacer()
                     switch song.difficulty {
                     case "easy":
@@ -64,55 +58,12 @@ struct SongView: View {
                         }
                         .padding(.trailing, 40)
                     }
-//                }
             }
             .background(Color(hex: 0x935FA7, opacity: 1))
             .frame(width: 720, height: 200)
             .cornerRadius(40)
             .shadow(radius: 16)
         }
-//        VStack(spacing: 10) {
-//            switch song.category {
-//            case "k-pop":
-//                Text(song.name)
-//            case "meme":
-//                Text(song.name)
-//            default:
-//                Text(song.name)
-//            }
-//            ZStack {
-//                Rectangle()
-//                    .foregroundColor(.white)
-//                    .frame(width: 121, height: 34)
-//                    .cornerRadius(20)
-//                HStack{
-//                    switch song.difficulty {
-//                    case "easy":
-//                        Image(systemName: "star.fill")
-//                            .foregroundColor(.orange)
-//                        Image(systemName: "star.fill")
-//                            .foregroundColor(.orange)
-//                    case "medium":
-//                        Image(systemName: "star.fill")
-//                            .foregroundColor(.orange)
-//                        Image(systemName: "star.fill")
-//                            .foregroundColor(.orange)
-//                        Image(systemName: "star.fill")
-//                            .foregroundColor(.orange)
-//                    case "hard":
-//                        Image(systemName: "star.fill")
-//                            .foregroundColor(.purple)
-//                        Image(systemName: "star.fill")
-//                            .foregroundColor(.purple)
-//                        Image(systemName: "star.fill")
-//                            .foregroundColor(.purple)
-//                    default:
-//                        Image(systemName: "star.fill")
-//                            .foregroundColor(.orange)
-//                    }
-//                }
-//            }
-//        }
     }
 }
 
