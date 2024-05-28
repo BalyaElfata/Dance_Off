619d831de5e2204c92022199ddd47446f20118af
Balya Elfata <balyaelfata@yahoo.com>
2024-05-28 14:47:27 +0700

    added new songs

--- Dance Off/Model/SongList.swift
+++ Dance Off/Model/SongList.swift
@@ -14,27 +14,19 @@ struct SongList: Identifiable, Hashable {
     var difficulty: String
     var songName: String
     
-//    init(id: UUID = UUID(), fileName: String, role: String, rarity: String, imageName: String) {
-//        self.fileName = fileName
-//        self.role = role
-//        self.category = category
-//        self.difficulty = difficulty
-//        self.songName = songName
-//    }
-    
     static func getSongData() -> [SongList] {
         return [
             SongList(fileName: "spongebob", category: "meme", difficulty: "hard", songName: "Spongebob Dance"),
             SongList(fileName: "gangnam_style", category: "meme", difficulty: "easy", songName: "Gangnam Style"),
             SongList(fileName: "harlem_shake", category: "meme", difficulty: "easy", songName: "Harlem Shake"),
-            SongList(fileName: "watch_me", category: "meme", difficulty: "medium", songName: "Watch Me"),
+            SongList(fileName: "watch_me", category: "meme", difficulty: "easy", songName: "Watch Me"),
             SongList(fileName: "caramelldansen", category: "meme", difficulty: "medium", songName: "Caramelldansen"),
             SongList(fileName: "permission_to_dance", category: "dance", difficulty: "hard", songName: "Permission To Dance"),
             SongList(fileName: "bagas_dribble", category: "meme", difficulty: "easy", songName: "Bagas Dribble"),
             SongList(fileName: "what_it_is", category: "dance", difficulty: "hard", songName: "What It Is"),
-            SongList(fileName: "cupid", category: "dance", difficulty: "hard", songName: "Cupid"),
-            SongList(fileName: "fortune_cookie", category: "dance", difficulty: "hard", songName: "Fortune Cookie"),
-            SongList(fileName: "joget_keju", category: "meme", difficulty: "hard", songName: "Joget Keju"),
+            SongList(fileName: "cupid", category: "dance", difficulty: "medium", songName: "Cupid"),
+            SongList(fileName: "fortune_cookie", category: "dance", difficulty: "medium", songName: "Fortune Cookie"),
+            SongList(fileName: "joget_keju", category: "meme", difficulty: "easy", songName: "Joget Keju"),
             SongList(fileName: "magnetic", category: "dance", difficulty: "hard", songName: "Magnetic"),
             SongList(fileName: "signal", category: "dance", difficulty: "hard", songName: "Signal"),
             SongList(fileName: "what_is_love", category: "dance", difficulty: "hard", songName: "What is Love?"),
