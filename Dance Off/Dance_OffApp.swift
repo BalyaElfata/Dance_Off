52603102a5f839924acabc7da7d24e2993de80e8
Balya Elfata <balyaelfata@yahoo.com>
2024-05-28 14:49:02 +0700

    added todo task from peer mentoring

--- Dance Off/Dance_OffApp.swift
+++ Dance Off/Dance_OffApp.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Balya Elfata on 14/05/24.
 //
+//TO DO: overall code nya sudah bagus dibagi MVVM, tapi agak bingung waktu baca bagian Video Capture nya sama hubungan Pose, Action classifier, bagian songnya juga sudah masuk enum dan ga pakai if else, jadi runnya lebih ringan cmiiw, keren Balya
 
 import SwiftUI
 
